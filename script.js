8fe18f9a9ccf678e83808ba82dda48a44a6dc72b
Arkana Play <arkanazone21@gmail.com>
2025-08-10 16:44:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 imgElement.alt = image.title; // Usando el campo "title" en tu JSON
                 imgElement.classList.add('scroll-item');
 
-                scrollWrapper.appendChild(imgElement);
+                // Crear el enlace <a> para las portadas moviles
+            const enlaceMobile = document.createElement('a');                    // Crea el enlace
+            enlaceMobile.href = image.url;                                       // Poner la URL que permite acceder la imagen de la pelicula
+            enlaceMobile.target = '_self';                                       // Opcional: abrir en la misma pestaña
+
+           enlaceMobile.appendChild(imgElement);                                 // Poner la imagen dentro del enlace
+           scrollWrapper.appendChild(enlaceMobile);                              // Agregar el enlace (con imagen) al contenedor
             });
 
             // Duplicar las imágenes para crear el efecto de bucle infinito
@@ -24,8 +30,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 imgElement.src = image.image;
                 imgElement.alt = image.title;
                 imgElement.classList.add('scroll-item');
+                
+// Crear el enlace <a> para las portadas moviles
+            const enlaceMobile = document.createElement('a');                    // Crea el enlace
+            enlaceMobile.href = image.url;                                       // Poner la URL que permite acceder la imagen de la pelicula
+            enlaceMobile.target = '_self';                                       // Opcional: abrir en la misma pestaña
 
-                scrollWrapper.appendChild(imgElement);
+           enlaceMobile.appendChild(imgElement);                                 // Poner la imagen dentro del enlace
+           scrollWrapper.appendChild(enlaceMobile);                              // Agregar el enlace (con imagen) al contenedor
             });
         })
         .catch(error => {
